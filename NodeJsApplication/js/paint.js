5b7ee1bf8fdf8642a806cfcb93f78320d9bae138
thethoron <melchior.vila@laposte.net>
2017-03-26 00:23:10 +0100

    paint v1

--- NodeJsApplication/js/paint.js
+++ NodeJsApplication/js/paint.js
@@ -1,11 +1,81 @@
 var my_canvas = document.getElementById("canvas");
 var context = my_canvas.getContext("2d");
-context.beginPath();
-context.arc(75,75,60,0,Math.PI*2);
-context.moveTo(20, 20);
-context.lineTo(100, 20);
-context.fillStyle = "red";
-context.fillRect(50, 60, 50, 20);
-context.font = "30px Garamond";
-context.fillText("Hello!",15,175);
-context.stroke();
+var points = [];
+
+var figures = [];
+
+function drawPoint(x, y)
+{
+	context.moveTo(x-5, y);
+	context.lineTo(x+5, y);
+	context.moveTo(x, y-5);
+	context.lineTo(x, y+5);
+}
+
+
+function drawCurrentPoints()
+{
+	if(points.length > 0)
+	{
+		context.beginPath();
+			drawPoint(points[0].x, points[0].y);
+			for(i=1; i < points.length; i++)
+				drawPoint(points[i].x, points[i].y);
+		context.stroke();
+		
+		context.beginPath();		
+			context.setLineDash([1,4]);
+			context.moveTo(points[0].x, points[0].y);
+			for(i=1; i < points.length; i++)
+				context.lineTo(points[i].x, points[i].y);
+		context.stroke();
+	}
+}
+
+function draw()
+{
+	context.clearRect(0, 0, my_canvas.width, my_canvas.height);
+	context.setLineDash([]);
+	context.beginPath();
+	for(i=0; i < figures.length; i++)
+	{
+		context.moveTo(figures[i][0].x, figures[i][0].y);
+        for(j=1; j < figures[i].length; j++)
+		{
+			context.lineTo(figures[i][j].x, figures[i][j].y);
+        }
+    }
+	context.stroke();
+	drawCurrentPoints();
+}
+
+function printMousePos(event)
+{
+	var newPoint = {x:event.clientX, y:event.clientY};
+	points.push(newPoint);
+	draw();
+}
+
+function validateBtn()
+{
+	figures.push(points);
+	points = [];	
+	draw();
+}
+
+function returnBtn()
+{
+	points = [];	
+	draw();
+}
+
+function deleteBtn()
+{
+	figures = [];
+	draw();
+}
+
+document.addEventListener("click", printMousePos);
+document.getElementById('validatebtn').addEventListener('click', validateBtn);
+document.getElementById('returnbtn').addEventListener('click', returnBtn);
+document.getElementById('deletebtn').addEventListener('click', deleteBtn);
