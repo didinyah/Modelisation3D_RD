7b390cc9348efc10939848d67535050a622f0c41
didinyah <dylanheitz@hotmail.fr>
2017-03-26 05:19:37 +0200

    Meilleure interface et prog de 2 boutons, plus que le dessin à la main

--- NodeJsApplication/js/paint.js
+++ NodeJsApplication/js/paint.js
@@ -12,88 +12,114 @@ window.onload = window.onresize = function() {
 
 function drawPoint(x, y)
 {
-	context.moveTo(x-5, y);
-	context.lineTo(x+5, y);
-	context.moveTo(x, y-5);
-	context.lineTo(x, y+5);
+    context.moveTo(x-5, y);
+    context.lineTo(x+5, y);
+    context.moveTo(x, y-5);
+    context.lineTo(x, y+5);
 }
 
 
 function drawCurrentPoints()
 {
-	if(points.length > 0)
-	{
-		context.beginPath();
-			drawPoint(points[0].x, points[0].y);
-			for(i=1; i < points.length; i++)
-				drawPoint(points[i].x, points[i].y);
-		context.stroke();
-		
-		context.beginPath();		
-			context.setLineDash([1,4]);
-			context.moveTo(points[0].x, points[0].y);
-			for(i=1; i < points.length; i++)
-				context.lineTo(points[i].x, points[i].y);
-		context.stroke();
-	}
+    if(points.length > 0)
+    {
+        context.beginPath();
+        drawPoint(points[0].x, points[0].y);
+        for(i=1; i < points.length; i++)
+                drawPoint(points[i].x, points[i].y);
+        context.stroke();
+
+        context.beginPath();		
+        context.setLineDash([1,4]);
+        context.moveTo(points[0].x, points[0].y);
+        for(i=1; i < points.length; i++)
+                context.lineTo(points[i].x, points[i].y);
+        context.stroke();
+    }
 }
 
 function draw()
 {
-	context.clearRect(0, 0, my_canvas.width, my_canvas.height);
-	context.setLineDash([]);
-	context.beginPath();
-	for(i=0; i < figures.length; i++)
-	{
-		context.moveTo(figures[i][0].x, figures[i][0].y);
+    context.clearRect(0, 0, my_canvas.width, my_canvas.height);
+    context.setLineDash([]);
+    context.beginPath();
+    for(i=0; i < figures.length; i++)
+    {
+        context.moveTo(figures[i][0].x, figures[i][0].y);
         for(j=1; j < figures[i].length; j++)
-		{
-			context.lineTo(figures[i][j].x, figures[i][j].y);
+        {
+            context.lineTo(figures[i][j].x, figures[i][j].y);
         }
     }
-	context.stroke();
-	drawCurrentPoints();
+    context.stroke();
+    drawCurrentPoints();
 }
 
 function printMousePos(event)
 {
-	var newPoint = {x:event.clientX, y:event.clientY};
-	points.push(newPoint);
-	draw();
+    var newPoint = {x:event.clientX, y:event.clientY};
+    points.push(newPoint);
+    draw();
 }
 
 function validateBtn()
 {
-	figures.push(points);
-	points = [];	
-	draw();
+    if(points.length>0) {
+       figures.push(points);
+        points = []; 
+    }
+    draw();
 }
 
 function returnBtn()
 {
-	points = [];	
-	draw();
+    points = [];	
+    draw();
+}
+
+// Ferme un polygone et valide
+function closeBtn()
+{
+    // au moins un triangle
+    if(points.length > 2) {
+        points.push(points[0]);
+        draw();
+    }
+}
+
+function deleteFigureBtn()
+{
+    if(figures.length > 0) {
+        figures.pop();
+    }
+    else {
+        figures = [];
+    }
+    draw();
 }
 
 function deleteBtn()
 {
-	figures = [];
-	draw();
-}
-function gestionMainOuLignes() {
-    
-}
-if ($("#handcb").prop( "checked" )) {
-    // on dessine à main levée ici
-    
-}
-else {
-    // on fait des lignes droites ici
-    
+    figures = [];
+    draw();
 }
 
+function gestionMainOuLignes() {
+    if ($("#handcb").prop( "checked" )) {
+        // on dessine à main levée ici
+    
+    }
+    else {
+        // on fait des lignes droites ici
+
+    }
+}
+
+// Ajout des évenements
 $("#mycanvas").on("click", printMousePos);
 $("#validatebtn").on('click', validateBtn);
 $("#returnbtn").on('click', returnBtn);
+$("#closebtn").on('click', closeBtn);
+$("#deletefigurebtn").on('click', deleteFigureBtn);
 $("#deletebtn").on('click', deleteBtn);
 $("#handcb").change(gestionMainOuLignes);
