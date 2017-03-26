8fe8a42a52ad2f936e9b0bc693f1abafd365b2a9
thethoron <melchior.vila@laposte.net>
2017-03-26 15:10:08 +0200

    paint v2

--- NodeJsApplication/js/paint.js
+++ NodeJsApplication/js/paint.js
@@ -1,8 +1,9 @@
 var my_canvas = document.getElementById("mycanvas");
 var context = my_canvas.getContext("2d");
 var points = [];
-
 var figures = [];
+var isHandDrawing = true;
+var mouseClicked = false;
 
 window.onload = window.onresize = function() {
     my_canvas = document.getElementById("mycanvas");
@@ -10,6 +11,7 @@ window.onload = window.onresize = function() {
     my_canvas.height = window.innerHeight;
 }
 
+//dessine un point
 function drawPoint(x, y)
 {
     context.moveTo(x-5, y);
@@ -18,8 +20,22 @@ function drawPoint(x, y)
     context.lineTo(x, y+5);
 }
 
+//dessine les tracé a main levée
+function handDrawing()
+{
+    if(points.length > 0)
+    {
+        context.beginPath();
+        context.setLineDash([1,1]);
+        context.moveTo(points[0].x, points[0].y);
+        for(i=1; i < points.length; i++)
+                context.lineTo(points[i].x, points[i].y);
+        context.stroke();
+    }
+}
 
-function drawCurrentPoints()
+//dessine les tracé droit
+function straightsDrawing()
 {
     if(points.length > 0)
     {
@@ -38,6 +54,16 @@ function drawCurrentPoints()
     }
 }
 
+//dessine le tracé en cours
+function drawCurrentPoints()
+{
+    if(isHandDrawing)
+		handDrawing();
+	else
+		straightsDrawing();
+}
+
+//dessine la scene
 function draw()
 {
     context.clearRect(0, 0, my_canvas.width, my_canvas.height);
@@ -55,13 +81,72 @@ function draw()
     drawCurrentPoints();
 }
 
-function printMousePos(event)
+
+//magnétisme sur les points
+function magnetism(pt)
 {
-    var newPoint = {x:event.clientX, y:event.clientY};
-    points.push(newPoint);
-    draw();
+	var distMagn = 25;
+    for(i=0; i < points.length; i++)
+	{
+			var dist = Math.sqrt(Math.pow(points[i].x - pt.x, 2) + Math.pow(points[i].y - pt.y, 2));
+			if(dist < distMagn)
+				return points[i];
+	}
+    for(i=0; i < figures.length; i++)
+    {
+        for(j=0; j < figures[i].length; j++)
+        {
+			var dist = Math.sqrt(Math.pow(figures[i][j].x - pt.x, 2) + Math.pow(figures[i][j].y - pt.y, 2));
+			if(dist < distMagn)
+				return figures[i][j];
+        }
+    }
+	return pt;
 }
 
+
+//ajoute un point au click
+function moveMouse(event)
+{
+	if(isHandDrawing)
+	{
+		if(mouseClicked)
+		{
+			var newPoint = {x:event.clientX, y:event.clientY};
+			points.push(newPoint);
+			draw();
+		}
+	}
+}
+
+//ajoute un point au click
+function clickMouse(event)
+{
+	var newPoint = {x:event.clientX, y:event.clientY};
+	if(isHandDrawing)
+	{
+		if(!mouseClicked)
+		{
+			mouseClicked = true;
+			newPoint = magnetism(newPoint);
+			draw();
+		}
+		else
+		{
+			mouseClicked = false;
+		points.push(newPoint);
+			validateBtn();
+		}
+	}
+	else
+	{
+		newPoint = magnetism(newPoint);
+		points.push(newPoint);
+		draw();
+	}
+}
+
+//sauvegarde le tracé en cours
 function validateBtn()
 {
     if(points.length>0) {
@@ -71,6 +156,7 @@ function validateBtn()
     draw();
 }
 
+//détruit le tracé en cours
 function returnBtn()
 {
     points = [];	
@@ -78,14 +164,15 @@ function returnBtn()
 }
 
 // Ferme un polygone et valide
-function closeBtn()
+//innutile si magnétisme
+/*function closeBtn()
 {
     // au moins un triangle
     if(points.length > 2) {
         points.push(points[0]);
         draw();
     }
-}
+}*/
 
 function deleteFigureBtn()
 {
@@ -104,22 +191,18 @@ function deleteBtn()
     draw();
 }
 
-function gestionMainOuLignes() {
-    if ($("#handcb").prop( "checked" )) {
-        // on dessine à main levée ici
-    
-    }
-    else {
-        // on fait des lignes droites ici
-
-    }
+function toggleHandDrawing()
+{
+	isHandDrawing = $("#handcb").prop( "checked" );
+	mouseClicked = false;
 }
 
 // Ajout des évenements
-$("#mycanvas").on("click", printMousePos);
+$("#mycanvas").on("mousemove", moveMouse);
+$("#mycanvas").on("click", clickMouse);
 $("#validatebtn").on('click', validateBtn);
 $("#returnbtn").on('click', returnBtn);
-$("#closebtn").on('click', closeBtn);
+//$("#closebtn").on('click', closeBtn);
 $("#deletefigurebtn").on('click', deleteFigureBtn);
 $("#deletebtn").on('click', deleteBtn);
-$("#handcb").change(gestionMainOuLignes);
+$("#handcb").change(toggleHandDrawing);
