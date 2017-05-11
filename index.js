3b89cf10dd55237325ad9624e7bb38a600e1e782
MrYawe <yannis.weishaupt@gmail.com>
2017-05-12 01:40:52 +0200

    comment Shape

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var detector = new ShapeDetector(ShapeDetector.defaultShapes);
+// var detector = new ShapeDetector(ShapeDetector.defaultShapes);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/mainpage.html');
@@ -17,12 +17,12 @@ io.on('connection', function(socket){
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
-    
+
     socket.on('data_message', function(data){
         console.log(data);
         // data = array of figures
         var figure1 = data[0];
-        var figure_reconnue = detector.spot(figure1);
+        // var figure_reconnue = detector.spot(figure1);
         io.emit('data_message', data);
         io.emit('figure_detection', figure_reconnue)
     });
