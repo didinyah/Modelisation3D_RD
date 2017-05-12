902d67fc6f82073224cc227af8a18776eda0f881
didinyah <dylanheitz@hotmail.fr>
2017-05-12 03:05:17 +0200

    Test de shape detector

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-//var detector = require('shape-detector');
+var detector = require('shape-detector');
 //var detector = new ShapeDetector(ShapeDetector.defaultShapes);
 
 app.get('/', function(req, res){
@@ -23,8 +23,8 @@ io.on('connection', function(socket){
         console.log(data);
         // data = array of figures
         var figure1 = data[0];
-        //var figure_reconnue = detector.spot(figure1);
-        var figure_reconnue = "cercle";
+        var figure_reconnue = detector.spot(figure1);
+        //var figure_reconnue = "cercle";
         io.emit('data_message', data);
         io.emit('figure_detection', figure_reconnue);
     });
